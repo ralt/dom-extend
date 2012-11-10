7287bce9507e69803f7ce86626853363f4094847
Florian Margaine <florian@margaine.com>
2012-11-10 09:12:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+dom-extend
+===
+
+Extending the DOM is bad. But this is so good:
+
+    document.querySelector(selector).forEach(function(el) {
+        // stuff
+    });
+
+More information: http://margaine.com/2012/10/31/loop-on-dom-elements.html
+
+Contributors
+---
+
+- [Florian Margaine](http://margaine.com)
+
+License
+---
+
+MIT License.
---
+++ dom-extend.js
@@ -0,0 +1,5 @@
+HTMLCollection.prototype.forEach = NodeList.prototype.forEach = Array.prototype.forEach;
+HTMLCollection.prototype.map = NodeList.prototype.map = Array.prototype.map;
+HTMLCollection.prototype.filter = NodeList.prototype.filter = Array.prototype.filter;
+HTMLCollection.prototype.every = NodeList.prototype.every = Array.prototype.every;
+HTMLCollection.prototype.some = NodeList.prototype.some = Array.prototype.some;
